3d83edcb6c11e270df1b62cf791476f563282c07
Michiel de Jong <michiel@unhosted.org>
2020-10-20 20:21:53 +0200

    git clone and git checkout

--- test/system/run-solid-test-suite.sh
+++ test/system/run-solid-test-suite.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -i
 npm start &
+PID=$!
 git clone https://github.com/solid/solid-crud-tests
 cd solid-crud-tests
+git checkout v0.1.0
 npm install
-# needed due to git dependency, until latest rdflib code is on npm:
-cd node_modules/rdflib && npm install && npm run build && cd ../..
 export SERVER_ROOT=http://localhost:3000
 npm run jest
+kill $PID
