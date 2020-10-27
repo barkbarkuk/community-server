6c6b2c1aca1606e9539dc5d914daf8d3498fb570
Michiel de Jong <michiel@unhosted.org>
2020-10-27 16:43:14 +0100

    Create ALICE_WEBID profile

--- test/system/run-solid-test-suite.sh
+++ test/system/run-solid-test-suite.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -i
+set -e
 npm start &
 PID=$!
-git clone https://github.com/solid/solid-crud-tests
+# git clone https://github.com/solid/solid-crud-tests
 cd solid-crud-tests
-git checkout v0.1.0
+git checkout v0.1.1
 npm install
 export SERVER_ROOT=http://localhost:3000
+export ALICE_WEBID=http://localhost:3000/profile.ttl
+curl -X PUT $ALICE_WEBID -d '<#me> <http://www.w3.org/ns/pim/space#storage> </>.'
 npm run jest
 kill $PID
